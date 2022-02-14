b3d83bd6b12bca202e81d13b250c2c805bbb329b
MertCelik123 <mertcelik2399@gmail.com>
2022-02-15 02:42:26 +0300

    Photo api ve identity server implementasyonlari tamamlandi. Basket api icin dto ve servisler kuruldu

---
+++ Services/Basket/Basket.Api/Service/IBasketService.cs
@@ -0,0 +1,19 @@
+﻿using Basket.Api.Dtos;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.Api.Service
+{
+    public interface IBasketService
+    {
+        Task<Response<BasketDto>> GetBasket(string userId);
+
+        Task<Response<bool>> SaveOrUpdate(BasketDto basketDto);
+
+        Task<Response<bool>> Delete(string userId);
+
+    }
+}
